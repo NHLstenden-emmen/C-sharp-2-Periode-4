861cfe44d748998739a185fbc9326a4fd83bf0c4
DaanDaling <73099939+DaanDaling@users.noreply.github.com>
2022-05-05 11:24:07 +0200

    - Update game
    - basis speler toegevoegd ( met tijdelijke ronde sprite)
    - basis controls toegevoegd (W A S D)
    - basis physics randen toegevoegd (speler kan niet buiten venster treden)

--- project/TopDownRacer/TopDownRacer/Game1.cs
+++ project/TopDownRacer/TopDownRacer/Game1.cs
@@ -6,6 +6,13 @@ namespace TopDownRacer
 {
     public class Game1 : Game
     {
+        //Declaring a variable of type Texture2D to add an image to
+        Texture2D playerTexture;
+
+        //Declaring a vector and variable to track player position and player speed
+        Vector2 playerPosition;
+        float playerSpeed;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -20,12 +27,20 @@ namespace TopDownRacer
         {
             // TODO: Add your initialization logic here
 
+            //Initialize the player speed and position
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2,
+                _graphics.PreferredBackBufferHeight / 2);
+            playerSpeed = 200f;
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Declare a texture to the variable, Ball is a temporary texture
+            playerTexture = Content.Load<Texture2D>("ball");
 
             // TODO: use this.Content to load your game content here
         }
@@ -37,6 +52,33 @@ namespace TopDownRacer
 
             // TODO: Add your update logic here
 
+            //Declaring basic player controls
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.W))
+                playerPosition.Y -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.S))
+                playerPosition.Y += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.A))
+                playerPosition.X -= playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.D))
+                playerPosition.X += playerSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+
+            //Declaring basic physics so the player does not go out of bounds
+            if (playerPosition.X > _graphics.PreferredBackBufferWidth - playerTexture.Width / 2)
+                playerPosition.X = _graphics.PreferredBackBufferWidth - playerTexture.Width / 2;
+            else if (playerPosition.X < playerTexture.Width / 2)
+                playerPosition.X = playerTexture.Width / 2;
+
+            if (playerPosition.Y > _graphics.PreferredBackBufferHeight - playerTexture.Height / 2)
+                playerPosition.Y = _graphics.PreferredBackBufferHeight - playerTexture.Height / 2;
+            else if (playerPosition.Y < playerTexture.Height / 2)
+                playerPosition.Y = playerTexture.Height / 2;
+
             base.Update(gameTime);
         }
 
@@ -45,6 +87,12 @@ namespace TopDownRacer
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            //Draw the car in the game
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(playerTexture, playerPosition, null, Color.White, 0f, new Vector2(playerTexture.Width / 2, playerTexture.Height / 2),
+            Vector2.One, SpriteEffects.None, 0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
