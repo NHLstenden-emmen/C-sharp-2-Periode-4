5ee532bad804ea02c0b5e1cd3e752eecc0332837
Kevin Smulders <smulders.kevin.1999@gmail.com>
2022-05-16 15:22:52 +0200

    meerdere spelers

--- TopDownRacer/Sprites/Sprite.cs
+++ TopDownRacer/Sprites/Sprite.cs
@@ -3,19 +3,32 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TopDownRacer.Models;
 
 namespace TopDownRacer.Sprites
 {
-    public class Sprite : Component
+    public class Sprite
     {
-
         protected Texture2D _texture;
-        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        public Vector2 Position;
+        public Vector2 Origin;
+        public Color Color = Color.White;
+        public Input Input;
+        public float Rotation;
+
+        public Sprite(Texture2D texture)
         {
-            throw new NotImplementedException();
+            _texture = texture;
         }
 
-        public override void Update(GameTime gameTime)
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            Origin = new Vector2(_texture.Width / 2, _texture.Height / 2);
+            spriteBatch.Draw(_texture, Position, null, Color, Rotation, Origin, 1, SpriteEffects.None, 0f);
+        }
+
+        public virtual void Update(GameTime gameTime, List<Sprite> sprites)
         {
 
         }
