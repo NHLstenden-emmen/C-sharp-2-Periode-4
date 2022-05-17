2b06b93fb2b20c82b346890c97f0147fdfadc490
Kevin Smulders <smulders.kevin.1999@gmail.com>
2022-05-17 19:26:14 +0200

    bumperlogica verplaatst naar de sprite class

--- TopDownRacer/Sprites/Sprite.cs
+++ TopDownRacer/Sprites/Sprite.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System;
 using TopDownRacer.Models;
 
 namespace TopDownRacer.Sprites
@@ -15,6 +16,22 @@ namespace TopDownRacer.Sprites
         public float Rotation = 0;
         public float CurrentPositionSpeed = 0;
 
+        public Rectangle sourceRectangle;
+
+        public delegate Vector2 getCornerCoords(Sprite sprite);
+
+        public getCornerCoords getCornerCoordsDel;
+        public int orientation;
+        public int height;
+        public int width;
+
+        public Rectangle Rectangle
+        {
+            get
+            {
+                return new Rectangle((int)Position.X, (int)Position.Y, width, height);
+            }
+        }
         public Sprite(Texture2D texture)
         {
             _texture = texture;
@@ -29,6 +46,165 @@ namespace TopDownRacer.Sprites
 
         public virtual void Update(GameTime gameTime, List<Sprite> sprites)
         {
+        }
+
+
+        public Vector2 getCornerCoordsTop(Sprite sprite)
+        {
+            // calculate the orientation and choose which method to call
+            var direction = new Vector2((float)Math.Cos(sprite.Rotation), (float)Math.Sin(sprite.Rotation));
+            if (direction.Y < 0)
+            {
+                if (direction.X < 0)
+                {
+                    return cornerCoordFR(sprite);
+                }
+                else
+                {
+                    return cornerCoordFL(sprite);
+                }
+
+            }
+            else
+            {
+                if (direction.X < 0)
+                {
+                    return cornerCoordBR(sprite);
+                }
+                else
+                {
+                    return cornerCoordBL(sprite);
+                }
+            }
+        }
+        public Vector2 getCornerCoordsRight(Sprite sprite)
+        {
+            // calculate the orientation and choose which method to call
+            var direction = new Vector2((float)Math.Cos(sprite.Rotation), (float)Math.Sin(sprite.Rotation));
+            if (direction.X > 0)
+            {
+                if (direction.Y < 0)
+                {
+                    return cornerCoordFR(sprite);
+                }
+                else
+                {
+                    return cornerCoordFL(sprite);
+                }
+
+            }
+            else
+            {
+                if (direction.Y < 0)
+                {
+                    return cornerCoordBR(sprite);
+                }
+                else
+                {
+                    return cornerCoordBL(sprite);
+                }
+            }
+        }
+        public Vector2 getCornerCoordsBottom(Sprite sprite)
+        {
+            // calculate the orientation and choose which method to call
+            var direction = new Vector2((float)Math.Cos(sprite.Rotation), (float)Math.Sin(sprite.Rotation));
+            if (direction.Y > 0)
+            {
+                if (direction.X < 0)
+                {
+                    return cornerCoordFR(sprite);
+                }
+                else
+                {
+                    return cornerCoordFL(sprite);
+                }
+
+            }
+            else
+            {
+                if (direction.X < 0)
+                {
+                    return cornerCoordBR(sprite);
+                }
+                else
+                {
+                    return cornerCoordBL(sprite);
+                }
+            }
+        }
+
+        public Vector2 getCornerCoordsLeft(Sprite sprite)
+        {
+            // calculate the orientation and choose which method to call
+            var direction = new Vector2((float)Math.Cos(sprite.Rotation), (float)Math.Sin(sprite.Rotation));
+            if (direction.X < 0)
+            {
+                if (direction.Y > 0)
+                {
+                    return cornerCoordFR(sprite);
+                }
+                else
+                {
+                    return cornerCoordFL(sprite);
+                }
+
+            }
+            else
+            {
+                if (direction.Y > 0)
+                {
+                    return cornerCoordBR(sprite);
+                }
+                else
+                {
+                    return cornerCoordBL(sprite);
+                }
+            }
+        }
+
+        private Vector2 cornerCoordFR(Sprite sprite)
+        {
+            // Corner front Right
+            float Ox = sprite._texture.Width / 2;
+            float Oy = sprite._texture.Height / 2;
+            float θ = MathHelper.ToDegrees(sprite.Rotation);
+            float Rx = (float)(sprite.Position.X + (Ox * Math.Cos(θ)) - (Oy * Math.Sin(θ)));
+            float Ry = (float)(sprite.Position.Y + (Ox * Math.Sin(θ)) + (Oy * Math.Cos(θ)));
+            return new Vector2(Rx, Ry);
+        }
+
+        private Vector2 cornerCoordFL(Sprite sprite)
+        {
+            // Corner Bottom Right
+            float Ox = sprite._texture.Width / 2;
+            float Oy = -sprite._texture.Height / 2;
+            float θ = MathHelper.ToDegrees(sprite.Rotation);
+            float Rx = (float)(sprite.Position.X + (Ox * Math.Cos(θ)) - (Oy * Math.Sin(θ)));
+            float Ry = (float)(sprite.Position.Y + (Ox * Math.Sin(θ)) + (Oy * Math.Cos(θ)));
+            return new Vector2(Rx, Ry);
+        }
+
+        private Vector2 cornerCoordBR(Sprite sprite)
+        {
+            // Corner Bottom Right
+            float Ox = -sprite._texture.Width / 2;
+            float Oy = sprite._texture.Height / 2;
+            float θ = MathHelper.ToDegrees(sprite.Rotation);
+            float Rx = (float)(sprite.Position.X + (Ox * Math.Cos(θ)) - (Oy * Math.Sin(θ)));
+            float Ry = (float)(sprite.Position.Y + (Ox * Math.Sin(θ)) + (Oy * Math.Cos(θ)));
+            return new Vector2(Rx, Ry);
+        }
+
+        private Vector2 cornerCoordBL(Sprite sprite)
+        {
+            // Corner Bottom Right
+            float Ox = -sprite._texture.Width / 2;
+            float Oy = -sprite._texture.Height / 2;
+            float θ = MathHelper.ToDegrees(sprite.Rotation);
+            float Rx = (float)(sprite.Position.X + (Ox * Math.Cos(θ)) - (Oy * Math.Sin(θ)));
+            float Ry = (float)(sprite.Position.Y + (Ox * Math.Sin(θ)) + (Oy * Math.Cos(θ)));
+            return new Vector2(Rx, Ry);
         }
     }
 }
