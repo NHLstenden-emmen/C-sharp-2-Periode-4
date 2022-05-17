2d9e3776c757844398e955f586d17a1ff992987e
Kevin Smulders <smulders.kevin.1999@gmail.com>
2022-05-17 19:26:27 +0200

    create finishline

---
+++ TopDownRacer/Sprites/Finishline.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TopDownRacer.Sprites
+{
+    class Finishline : Sprite
+    {
+
+        public Finishline(Texture2D texture, int orientation, int height, int width)
+        : base(texture)
+        {
+
+            this.orientation = orientation;
+            this.height = height;
+            this.width = width;
+            sourceRectangle = Rectangle;
+            switch (orientation)
+            {
+                case 0:
+                    getCornerCoordsDel = getCornerCoordsTop;
+                    break;
+                case 1:
+                    getCornerCoordsDel = getCornerCoordsRight;
+                    break;
+                case 2:
+                    getCornerCoordsDel = getCornerCoordsBottom;
+                    break;
+                case 3:
+                    getCornerCoordsDel = getCornerCoordsLeft;
+                    break;
+            }
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, Position, sourceRectangle, Color, Rotation, Vector2.Zero, 1, SpriteEffects.None, 0.9f);
+        }
+
+        public override void Update(GameTime gameTime, List<Sprite> sprites)
+        {
+            foreach (var sprite in sprites)
+            {
+                if (sprite is Player)
+
+                    if (!((Player)sprite).Dead)
+                    {
+                        // the car is moving right
+                        var coords = getCornerCoordsDel(sprite);
+                        if (coords.Y < this.Position.Y + height && coords.Y > this.Position.Y && coords.X < this.Position.X + width && coords.X > this.Position.X)
+                        {
+                            ((Player)sprite).Score += 1000;
+                            ((Player)sprite).Dead = true;
+                        }
+                    }
+            }
+        }
+
+    }
+}
