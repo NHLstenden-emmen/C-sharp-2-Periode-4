ad7377d1318640d4b9cd14e78872b6fc6a0a602e
Kevin Smulders <smulders.kevin.1999@gmail.com>
2022-05-23 22:06:20 +0200

    divide the draw and update to the right state

--- TopDownRacer/Game1.cs
+++ TopDownRacer/Game1.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using TopDownRacer.Models;
 using TopDownRacer.Sprites;
 using TopDownRacer.States;
 
@@ -16,13 +15,12 @@ namespace TopDownRacer
 
         public static int ScreenWidth = 1920;
         public static int ScreenHeight = 1080;
-        private static int TrackWidth = 400;
+        public static int TrackWidth = 400;
 
         //Declaring a variable of type Texture2D to add an image to
-        protected Texture2D checkpointTexture, bumperTexture, finishlineTexture;
-        private List<Sprite> _sprites;
+        public List<Sprite> _sprites;
 
-        Random rnd = new Random();
+        public Random rnd = new Random();
 
         private State _currentState;
 
@@ -55,93 +53,6 @@ namespace TopDownRacer
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            var playerTexture = new List<Texture2D> { };
-
-            playerTexture.Insert(0, Content.Load<Texture2D>("Player/car_small_1"));
-            playerTexture.Insert(1, Content.Load<Texture2D>("Player/car_small_2"));
-            playerTexture.Insert(2, Content.Load<Texture2D>("Player/car_small_3"));
-            playerTexture.Insert(3, Content.Load<Texture2D>("Player/car_small_4"));
-            playerTexture.Insert(4, Content.Load<Texture2D>("Player/car_small_5"));
-
-            bumperTexture = Content.Load<Texture2D>("Levels/tires_white");
-            finishlineTexture = Content.Load<Texture2D>("Levels/finishline");
-            checkpointTexture = Content.Load<Texture2D>("Levels/checkpoint");
-
-
-            _sprites = new List<Sprite>()
-              {
-                new Player(playerTexture[rnd.Next(playerTexture.Count)])
-                {
-                  Name = "Simchaja",
-                  Input = new Input(),
-                  Color = Color.Blue,
-                },
-                new Player(playerTexture[rnd.Next(playerTexture.Count)])
-                {
-                  Name = "Roan",
-                  Input = new Input()
-                  {
-                    Left = Keys.Left,
-                    Right = Keys.Right,
-                    Up = Keys.Up,
-                    Down = Keys.Down,
-                  },
-                  Color = Color.Green,
-                },
-                // outer ring
-                new Bumper(bumperTexture, 0, ScreenWidth,bumperTexture.Height)
-                {
-                    Position = new Vector2(0,0)
-                },
-                new Bumper(bumperTexture, 1, bumperTexture.Width, ScreenHeight - TrackWidth)
-                {
-                    Position = new Vector2(ScreenWidth - bumperTexture.Width, 0)
-                },
-                new Bumper(bumperTexture, 2, ScreenWidth, bumperTexture.Height)
-                {
-                    Position = new Vector2(0, ScreenHeight - bumperTexture.Height)
-                },
-                new Bumper(bumperTexture, 3,bumperTexture.Width, ScreenHeight)
-                {
-                    Position = new Vector2(0, 0)
-                },
-                // inner ring
-
-                new Bumper(bumperTexture, 0, ScreenWidth-TrackWidth*2, bumperTexture.Height)
-                {
-                    Position = new Vector2(TrackWidth,TrackWidth)
-                },
-                new Bumper(bumperTexture, 1, bumperTexture.Width, ScreenHeight - TrackWidth*2)
-                {
-                    Position = new Vector2(ScreenWidth - bumperTexture.Width - TrackWidth, TrackWidth)
-                },
-                new Bumper(bumperTexture, 2, ScreenWidth - TrackWidth * 2, bumperTexture.Height)
-                {
-                    Position = new Vector2(TrackWidth, ScreenHeight - bumperTexture.Height - TrackWidth)
-                },
-                new Bumper(bumperTexture, 3,bumperTexture.Width, ScreenHeight - TrackWidth * 2)
-                {
-                    Position = new Vector2(TrackWidth, TrackWidth)
-                },
-                // FinishLine
-                new Finishline(finishlineTexture, 1, finishlineTexture.Width, ScreenHeight)
-                {
-                    Position = new Vector2(ScreenWidth - finishlineTexture.Width, ScreenHeight - TrackWidth),
-                    amountCheckpoint = 2
-                },
-                // checkpoint's
-                new Checkpoint(checkpointTexture, 1, checkpointTexture.Width, TrackWidth)
-                {
-                    Position = new Vector2((ScreenWidth / 2) - checkpointTexture.Width, 0),
-                    checkpointId = 0
-                },
-                new Checkpoint(checkpointTexture, 1, checkpointTexture.Width, ScreenHeight)
-                {
-                    Position = new Vector2((ScreenWidth / 2) - checkpointTexture.Width, ScreenHeight - TrackWidth),
-                    checkpointId = 1
-                },
-            };
-
             _font = Content.Load<SpriteFont>("Fonts/Font");
             _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
         }
@@ -158,11 +69,6 @@ namespace TopDownRacer
                 _currentState = _nextState;
 
                 _nextState = null;
-            }
-
-            foreach (Sprite sprite in _sprites)
-            {
-                sprite.Update(gameTime, _sprites);
             }
 
             _currentState.Update(gameTime);
--- TopDownRacer/States/GameState.cs
+++ TopDownRacer/States/GameState.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using TopDownRacer.Models;
 using TopDownRacer.Sprites;
 
 namespace TopDownRacer.States
@@ -12,6 +14,92 @@ namespace TopDownRacer.States
         public GameState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content)
           : base(game, graphicsDevice, content)
         {
+            var playerTexture = new List<Texture2D> { };
+
+            playerTexture.Insert(0, content.Load<Texture2D>("Player/car_small_1"));
+            playerTexture.Insert(1, content.Load<Texture2D>("Player/car_small_2"));
+            playerTexture.Insert(2, content.Load<Texture2D>("Player/car_small_3"));
+            playerTexture.Insert(3, content.Load<Texture2D>("Player/car_small_4"));
+            playerTexture.Insert(4, content.Load<Texture2D>("Player/car_small_5"));
+
+            bumperTexture = content.Load<Texture2D>("Levels/tires_white");
+            finishlineTexture = content.Load<Texture2D>("Levels/finishline");
+            checkpointTexture = content.Load<Texture2D>("Levels/checkpoint");
+            backgroundTexture = content.Load<Texture2D>("Levels/background");
+
+            game._sprites = new List<Sprite>()
+              {
+                new Player(playerTexture[game.rnd.Next(playerTexture.Count)])
+                {
+                  Name = "Simchaja",
+                  Input = new Input(),
+                  Color = Color.Blue,
+                },
+                new Player(playerTexture[game.rnd.Next(playerTexture.Count)])
+                {
+                  Name = "Roan",
+                  Input = new Input()
+                  {
+                    Left = Keys.Left,
+                    Right = Keys.Right,
+                    Up = Keys.Up,
+                    Down = Keys.Down,
+                  },
+                  Color = Color.Green,
+                },
+                // outer ring
+                new Bumper(bumperTexture, 0, Game1.ScreenWidth,bumperTexture.Height)
+                {
+                    Position = new Vector2(0,0)
+                },
+                new Bumper(bumperTexture, 1, bumperTexture.Width, Game1.ScreenHeight - Game1.TrackWidth)
+                {
+                    Position = new Vector2(Game1.ScreenWidth - bumperTexture.Width, 0)
+                },
+                new Bumper(bumperTexture, 2, Game1.ScreenWidth, bumperTexture.Height)
+                {
+                    Position = new Vector2(0, Game1.ScreenHeight - bumperTexture.Height)
+                },
+                new Bumper(bumperTexture, 3,bumperTexture.Width, Game1.ScreenHeight)
+                {
+                    Position = new Vector2(0, 0)
+                },
+                // inner ring
+
+                new Bumper(bumperTexture, 0, Game1.ScreenWidth-Game1.TrackWidth*2, bumperTexture.Height)
+                {
+                    Position = new Vector2(Game1.TrackWidth,Game1.TrackWidth)
+                },
+                new Bumper(bumperTexture, 1, bumperTexture.Width, Game1.ScreenHeight - Game1.TrackWidth*2)
+                {
+                    Position = new Vector2(Game1.ScreenWidth - bumperTexture.Width - Game1.TrackWidth, Game1.TrackWidth)
+                },
+                new Bumper(bumperTexture, 2, Game1.ScreenWidth - Game1.TrackWidth * 2, bumperTexture.Height)
+                {
+                    Position = new Vector2(Game1.TrackWidth, Game1.ScreenHeight - bumperTexture.Height - Game1.TrackWidth)
+                },
+                new Bumper(bumperTexture, 3,bumperTexture.Width, Game1.ScreenHeight - Game1.TrackWidth * 2)
+                {
+                    Position = new Vector2(Game1.TrackWidth, Game1.TrackWidth)
+                },
+                // FinishLine
+                new Finishline(finishlineTexture, 1, finishlineTexture.Width, Game1.ScreenHeight)
+                {
+                    Position = new Vector2(Game1.ScreenWidth - finishlineTexture.Width, Game1.ScreenHeight - Game1.TrackWidth),
+                    amountCheckpoint = 2
+                },
+                // checkpoint's
+                new Checkpoint(checkpointTexture, 1, checkpointTexture.Width, Game1.TrackWidth)
+                {
+                    Position = new Vector2((Game1.ScreenWidth / 2) - checkpointTexture.Width, 0),
+                    checkpointId = 0
+                },
+                new Checkpoint(checkpointTexture, 1, checkpointTexture.Width, Game1.ScreenHeight)
+                {
+                    Position = new Vector2((Game1.ScreenWidth / 2) - checkpointTexture.Width, Game1.ScreenHeight - Game1.TrackWidth),
+                    checkpointId = 1
+                },
+            };
         }
 
         //Het starten van het spel
@@ -20,6 +108,8 @@ namespace TopDownRacer.States
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap, null, null);
 
             int fontY = 50;
+
+            spriteBatch.Draw(backgroundTexture, new Vector2(0, 0), Color.White);
 
             foreach (Sprite sprite in _sprites)
             {
@@ -46,7 +136,10 @@ namespace TopDownRacer.States
 
         public override void Update(GameTime gameTime)
         {
-            //kan later worden gebruikt
+            foreach (Sprite sprite in _game._sprites)
+            {
+                sprite.Update(gameTime, _game._sprites);
+            }
         }
     }
 }
