34e668ff1255d3dd07b6c2018c6a1ec938042df8
DaanDaling <73099939+DaanDaling@users.noreply.github.com>
2022-05-30 12:50:26 +0200

    Het neural network opgesteld
    - basis contrusctor van het neural network opgesteld
    - class gemaakt voor het network
    - functie om input layer te maken

---
+++ TopDownRacer/NeuralNetwork/RaceNeuralNetwork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TopDownRacer.NeuralNetwork.ActivationFunctions;
+using TopDownRacer.NeuralNetwork.InputFunctions;
+using TopDownRacer.NeuralNetwork.Layers;
+
+namespace TopDownRacer.NeuralNetwork
+{
+    //De implementatie van het neural network om de race game te spelen 
+    class RaceNeuralNetwork
+    {
+        //Eerst moet het mogelijk zijn om Neural Layers te maken dus maken we een factory aan
+        private NeuralLayerFactory _layerFactory;
+
+        //Hierna moeten we wat basis elementen opstellen die nodig zijn voor het netwerk om te functioneren
+
+        //een lijst met alle neural layers
+        internal List<NeuralLayer> _layers;
+
+        //een collectie van de errros van de neuronen aan de hand van een key/value paar
+        internal Dictionary<int, double[]> _neuronErrors;
+
+        //De learningrate en verwachte uitkomst 
+        internal double _learningRate;
+        internal double[][] _expectedResult; 
+
+        //de constructor van het neural network, waarbij de het aantal neurons in de input layer de input is
+        public RaceNeuralNetwork(int numberOfInputNeurons)
+        {
+            //initialiseren van de list, dictionary en factory
+            _layers = new List<NeuralLayer>();
+            _neuronErrors = new Dictionary<int, double[]>();
+            _layerFactory = new NeuralLayerFactory();
+
+            //ook moet een input layer worden aangemaakt om inputs te verzamelen
+            CreateInputLayer(numberOfInputNeurons);
+        }
+
+        //Functie die een input layer van het neural network maakt
+        private void CreateInputLayer(int numberOfInputNeurons)
+        {
+            var inputLayer = _layerFactory.CreateNeuralLayer(numberOfInputNeurons, new RectifiedActivationFuncion(), new WeightedSumFunction());
+            inputLayer.Neurons.ForEach(x => x.AddInputSynapse(0));
+            //Hier moet nog een methode komen om de layer toe te voegen 
+        }
+    }
+}
