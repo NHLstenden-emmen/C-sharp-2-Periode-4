797e9d9c96797879bd3ef1de8fa137621743b4a3
DaanDaling <73099939+DaanDaling@users.noreply.github.com>
2022-05-30 12:38:19 +0200

    Neural Layer
    - Neural layer factory aangemaakt
    - neural layer factory verplaatst naar layers folder

--- TopDownRacer/NeuralNetwork/Layers/NeuralLayerFactory.cs
+++ TopDownRacer/NeuralNetwork/Layers/NeuralLayerFactory.cs
@@ -1,18 +1,22 @@
-﻿using TopDownRacer.NeuralNetwork.ActivationFunctions;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TopDownRace.NeuralNetwork.Neuron;
+using TopDownRacer.NeuralNetwork.ActivationFunctions;
 using TopDownRacer.NeuralNetwork.InputFunctions;
 
-namespace TopDownRace.NeuralNetwork
+namespace TopDownRacer.NeuralNetwork.Layers
 {
-    //Factory voor het maken van layers
-    public class NeuralLayerFactory
+    class NeuralLayerFactory
     {
+        //Een factory die gebruikt kan worden om neural layers te maken
         public NeuralLayer CreateNeuralLayer(int numberOfNeurons, IActivationFunction activationFunction, IInputFunction inputFunction)
         {
             var layer = new NeuralLayer();
 
             for (int i = 0; i < numberOfNeurons; i++)
             {
-                var neuron = new Neuron.Neuron(activationFunction, inputFunction);
+                var neuron = new Neuron(activationFunction, inputFunction);
                 layer.Neurons.Add(neuron);
             }
 
