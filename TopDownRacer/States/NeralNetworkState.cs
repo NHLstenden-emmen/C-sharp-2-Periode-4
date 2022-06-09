e67261dd82e2492932235393bee3ec7edf853ecc
RoanMeijer <Roan.meijer10@gmail.com>
2022-06-09 10:40:41 +0200

    fixed front vision not working in current version
    Co-Authored-By: Daan Daling <73099939+DaanDaling@users.noreply.github.com>

--- TopDownRacer/States/NeralNetworkState.cs
+++ TopDownRacer/States/NeralNetworkState.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using TopDownRacer.Controller;
 using TopDownRacer.Sprites;
@@ -47,8 +48,12 @@ namespace TopDownRacer.States
                     // draw text on scoarboard
                     spriteBatch.DrawString(_font, string.Format("Player {0}: {1}", ((Player)sprite).Name, ((Player)sprite).Score), ScoreBoardPosition, ((Player)sprite).Color, 0, Vector2.Zero, 1, SpriteEffects.None, 0.4f);
                     if (!((Player)sprite).Dead)
+                    {
                         // draw player
                         sprite.Draw(spriteBatch, 0.6f);
+                        //draw closet barrier front indication
+                        spriteBatch.DrawString(_font, "00000", findClosestBarrierFront(sprite), Color.Red, 0, Vector2.Zero, 1, SpriteEffects.None, 0.4f);
+                    }
                 }
                 else
                 {
@@ -73,6 +78,39 @@ namespace TopDownRacer.States
             {
                 sprite.Update(gameTime, _game._sprites);
             }
+        }
+
+        private Vector2 findClosestBarrierFront(Sprite sprite)
+        {
+            int count = 0;
+
+            while (count < 1920 / 28)
+            {
+                count++;
+
+                //double yDif = Math.Tan(sprite.Rotation) * (sprite.Position.X  + count * (bumperTexture.Width / 2) - sprite.Position.X);
+                //double y = sprite.Position.Y - yDif;
+                double y = sprite.Position.Y + ((count * bumperTexture.Width / 2) * Math.Sin((sprite.Rotation)));
+
+                double x = sprite.Position.X + ((count * bumperTexture.Width / 2) * Math.Cos((sprite.Rotation)));
+
+                //Debug.WriteLine(x + ", " + y + " - " + MathHelper.ToDegrees(sprite.Rotation));
+
+                foreach (var sprite2 in _game._sprites)
+                {
+                    if (sprite2 is Bumper)
+                    {
+                        //Debug.WriteLine("car postion: " + x + ", " + y + " - Bumber: " + sprite.);
+
+                        if (y < sprite2.Position.Y + sprite2.height && y > sprite2.Position.Y && x < sprite2.Position.X + sprite2.width && x > sprite2.Position.X)
+                        {
+                            return new Vector2((float)x, (float)y);
+                        }
+                    }
+                }
+            }
+
+            return new Vector2(0, 0);
         }
     }
 }
